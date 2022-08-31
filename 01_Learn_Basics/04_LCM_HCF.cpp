e86aebe13a9b719227a7b57ed4982590d23934a4
Madhukar <madhukarkumarsocial@gmail.com>
2022-09-01 04:34:06 +0530

    LCM and HCF

---
+++ 01_Learn_Basics/04_LCM_HCF.cpp
@@ -0,0 +1,33 @@
+/*
+        #LCM and HCF
+
+        GfG :: https://practice.geeksforgeeks.org/problems/lcm-and-gcd4516/1
+
+*/
+
+// 1st : Using Euclidean’s  Theorem
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+// 2nd : Using C++ STL
+
+int gcd(int a, int b)
+{
+    return __gcd(a,b); // GCD of two numbers
+}
+
+int LCM (int a, int b)
+{
+    return lcm(a,b); // LCM of two numbers
+}
+
+// TC : O(logn)
+// SC : O(1)
+
