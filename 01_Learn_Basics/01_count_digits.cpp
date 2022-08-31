d8688a5d1e8e8b7f9a2be3cb2cecbee3b9b4ad8b
Madhukar <madhukarkumarsocial@gmail.com>
2022-09-01 03:54:40 +0530

    updated

--- 01_Learn_Basics/01_count_digits.cpp
+++ 01_Learn_Basics/01_count_digits.cpp
@@ -2,6 +2,9 @@
     Given a number N. Count the number of digits in N which evenly divides N.
 
     Question link :: https://practice.geeksforgeeks.org/problems/count-digits5716/1
+
+    TC : O(n)
+    SC : O(1)
 */
 
 int evenlyDivides(int N)
@@ -9,9 +12,10 @@ int evenlyDivides(int N)
 
     int copy = N;
     int count = 0;
+    int rem;
     while (copy > 0)
     {
-        int rem = copy % 10;
+         rem = copy % 10;
         if (rem != 0) // check if digit is zero
         {
             if (N % rem == 0) // if divisible then update count
---
+++ 01_Learn_Basics/02_reverse_integer.cpp
@@ -0,0 +1,8 @@
+/*
+    Given a 32 bit number X, reverse its binary form and print the answer in decimal.
+
+    Leetcode (Problem 7) :: https://leetcode.com/problems/reverse-integer/
+    GfG :: https://practice.geeksforgeeks.org/problems/reverse-bits3556/1
+
+*/
+
