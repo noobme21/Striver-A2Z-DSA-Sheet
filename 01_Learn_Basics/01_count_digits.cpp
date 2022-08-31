6f230f7b62488a51b49be8faa91f392c129639c1
Madhukar <madhukarkumarsocial@gmail.com>
2022-09-01 03:06:03 +0530

    first commit

---
+++ 01_Learn_Basics/01_count_digits.cpp
@@ -0,0 +1,21 @@
+/*
+    link ::    https://practice.geeksforgeeks.org/problems/count-digits5716/1
+*/
+
+int evenlyDivides(int N)
+{
+
+    int copy = N;
+    int count = 0;
+    while (copy > 0)
+    {
+        int rem = copy % 10;
+        if (rem != 0) // check if digit is zero
+        {
+            if (N % rem == 0) // if divisible then update count
+                count++;
+        }
+        copy = copy / 10;
+    }
+    return count;
+}
